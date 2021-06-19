63c287fb9354645cb5ceabe4d3f3c7ca078513b1
alexej <door1975@yandex.ru>
2021-06-20 00:01:38 +0300

    Убрал apache.tika и docx4j. Мешали статовать приложению, затягивали другой логгер. Убрал лишние тесты.

--- src/test/java/ru/durnov/docx/DocxTest.java
+++ src/test/java/ru/durnov/docx/DocxTest.java
@@ -1,27 +1,9 @@
 package ru.durnov.docx;
 
 import org.apache.batik.transcoder.TranscoderException;
-import org.apache.poi.xwpf.converter.IURIResolver;
-import org.apache.poi.xwpf.converter.IXWPFConverter;
-import org.apache.poi.xwpf.converter.internal.xhtml.XHTMLMapper;
-import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
-import org.apache.poi.xwpf.converter.xhtml.XWPF2XHTMLConverter;
 import org.apache.poi.xwpf.usermodel.*;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.metadata.MSOffice;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.microsoft.MSOwnerFileParser;
-import org.apache.tika.parser.microsoft.WordExtractor;
-import org.apache.tika.sax.ToXMLContentHandler;
-import org.docx4j.Docx4J;
-import org.docx4j.convert.out.html.AbstractHtmlExporter;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.junit.jupiter.api.Test;
-import org.xml.sax.SAXException;
+
 
 import java.io.*;
 import java.nio.file.Files;
@@ -74,42 +56,6 @@ class DocxTest {
         docx.archive().pathToArchive();
     }
 
-    @Test
-    void testConverter() throws Exception {
-        XWPFDocument xwpfDocument = new XWPFDocument(Files.newInputStream(Path.of("Test/Объединение ячеек.docx")));
-        IXWPFConverter<XHTMLOptions> converter = XWPF2XHTMLConverter.getInstance();
-        converter.convert(
-                xwpfDocument,
-                Files.newOutputStream(Path.of("Test/convert.html")),
-                new XHTMLOptions()
-        );
-    }
-
-    @Test
-    void apacheTikaTest() throws IOException, TikaException, SAXException {
-        InputStream inputStream = Files.newInputStream(Path.of("Test/prikaz1.docx"));
-        Parser parser = new AutoDetectParser();
-        Metadata metadata = new Metadata();
-        ToXMLContentHandler toXMLContentHandler = new ToXMLContentHandler();
-        ParseContext parseContext = new ParseContext();
-        parser.parse(inputStream, toXMLContentHandler, metadata, parseContext);
-        String html = toXMLContentHandler.toString();
-        System.out.println(html);
-        BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of("Test/tikaTest.html"));
-        bufferedWriter.write(html);
-        bufferedWriter.flush();
-    }
-
-    @Test
-    void testDocx4jConverting() throws IOException, Docx4JException {
-        WordprocessingMLPackage mlPackage = WordprocessingMLPackage.load(
-                Files.newInputStream(Path.of("Test/prikaz1.docx"))
-        );
-        OutputStream outputStream = Files.newOutputStream(Path.of("Test/testDocx4j.html"));
-        String imageDir = "Test/prikaz1.docx_files";
-        String imageTargetUri = "parikaz1.docx";
-        Docx4J.toHTML(mlPackage, imageDir, imageTargetUri, outputStream);
-    }
 
     @Test
     void testPrikaz1WithLinks() throws IOException, TranscoderException {
