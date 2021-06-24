d73edcab4ebbbc83bdf9098d620bd618f520ee51
alexdurnoff <door1975@yandex.ru>
2021-06-24 23:07:34 +0300

    Добавил тестовые docx-варианты файлов

--- src/test/java/ru/durnov/docx/DotNumbersInHeaderTest.java
+++ src/test/java/ru/durnov/docx/DotNumbersInHeaderTest.java
@@ -5,6 +5,7 @@ import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DotNumbersInHeaderTest {
